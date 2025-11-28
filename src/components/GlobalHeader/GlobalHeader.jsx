8fd06e7729e9a0aa6d8745019a95051033e48ccc
WendyDarli <139488528+WendyDarli@users.noreply.github.com>
2025-11-28 14:27:32 -0300

    feat: global header component

---
+++ src/components/GlobalHeader/GlobalHeader.jsx
@@ -0,0 +1,22 @@
+import './GlobalHeader.css';
+import starfyIcon from '../../assets/whiteIcons/starfy.svg';
+
+function GlobalHeader (){
+    return (
+        <div className='headerContainer'>
+            <img id='starfyIcon' src={starfyIcon} alt='starfy Icon'></img>
+            <div id='middleHeader'>
+                <button id='homeBttn' onClick={() => window.location.href = "/"} ></button>
+                <form>
+                    <button id='searchBttn' type='submit'></button>
+                    <input  id='searchInput' type='text' placeholder='What do you want to play?'></input>
+                    
+                </form>                
+            </div>
+
+            <button>Profile</button>
+        </div>
+    )
+};
+
+export default GlobalHeader;
