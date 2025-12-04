514cf3098710beca87269ba37107c33e5db93df0
WendyDarli <139488528+WendyDarli@users.noreply.github.com>
2025-12-04 13:27:15 -0300

    feat: main section component

---
+++ src/components/MainSection/MainSection.jsx
@@ -0,0 +1,13 @@
+import './MainSection.css';
+
+function MainSection() {
+
+    return (
+        <main className='mainSectionContainer'>
+            {/* decides which component to render based in the route */}
+            <p>Main Content</p>
+        </main>
+    );
+}
+
+export default MainSection;
--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,16 @@ import { createRoot } from 'react-dom/client'
 //COMPONENTS
 import GlobalHeader from './components/GlobalHeader/GlobalHeader.jsx'
 import LibrarySidebar from './components/LibrarySidebar/LibrarySidebar.jsx'
+import MainSection from './components/MainSection/MainSection.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <GlobalHeader />
     <div id='appLayout'>
       <LibrarySidebar />
+      <MainSection />      
     </div>
+
   </StrictMode>
 )
