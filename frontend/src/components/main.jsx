e4916e92b883be7a75c14a68ca28ba181317f24d
Wendy <139488528+WendyDarli@users.noreply.github.com>
2026-01-14 16:45:13 -0300

    setup: initial React Router routes

--- frontend/src/components/main.jsx
+++ frontend/src/components/main.jsx
@@ -1,6 +1,28 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router';
 import App from './App.jsx';
+import Home from './Home/Home.jsx';
+import UserProfile from './UserProfile/UserProfile.jsx';
+import CollectionDisplay from './CollectionDisplay/CollectionDisplay.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { path: 'home', element: <Home /> },
+      { path: 'profile', element: <UserProfile /> },
+      //add index for collection and playlist
+      { path: 'playlist/:id', element: <CollectionDisplay key='playlist'/> },
+      { path: 'collection/:id', element: <CollectionDisplay key='collection'/> },
+      { path: 'artist/:id', element: <CollectionDisplay key='artist'/> },
+      { path: 'album/:id', element: <CollectionDisplay key='album'/> },
+      { path: 'song/:id', element: <CollectionDisplay key='song'/> },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
