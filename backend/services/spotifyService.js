c996f65105d3aa14f0e29da6a15c65af73379513
Wendy <139488528+WendyDarli@users.noreply.github.com>
2026-01-24 20:22:18 -0300

    feat: add token refresh service

---
+++ backend/services/spotifyService.js
@@ -0,0 +1,43 @@
+const redisClient =  require('../redis');
+const axios = require('axios');
+require('dotenv').config();
+
+//this fn checks if token is valid, if not it refreshes the token and saves it to redis
+async function ensureValidSpotifyToken(userId){
+  const accessToken = await redisClient.get(`tokens:user:${userId}:access_token`);
+  const refreshToken = await redisClient.get(`tokens:user:${userId}:refresh_token`);
+
+  try{
+      await axios.get('https://api.spotify.com/v1/me', {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      });
+
+      return accessToken;
+
+  } catch(err){
+
+    try{
+      const params = new URLSearchParams({
+        grant_type: 'refresh_token',
+        refresh_token: refreshToken,
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET
+      });
+
+      const response = await axios.post('https://accounts.spotify.com/api/token', params, {
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+      });
+
+      const new_access_token = response.data.access_token;
+      await redisClient.set(`tokens:user:${userId}:access_token`, new_access_token, 'EX', 3600);
+      return new_access_token;
+    }catch(err){
+       
+      console.error('refresh token failed: ', err.response?.data);
+      console.log(refreshToken)
+      throw new Error('Spotify session expired. Please login again.');
+    }
+  }
+};
+
+module.exports = { ensureValidSpotifyToken };
