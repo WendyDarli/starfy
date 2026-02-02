415a658af425ee2884a7eec30d960c549e2837d6
Wendy <139488528+WendyDarli@users.noreply.github.com>
2026-02-02 15:02:40 -0300

    feat: update lyrics controller to get only plain lyrics

--- backend/controllers/lyricsController.js
+++ backend/controllers/lyricsController.js
@@ -5,28 +5,20 @@ async function lyrics_get(req, res) {
         const fetchLyrics = await axios.get('https://lrclib.net/api/get', {
             params: req.query,
         });
-        const plainLyrics = fetchLyrics.data.plainLyrics;
-        const syncedLyrics = fetchLyrics.data.syncedLyrics;
         
-        res.json({
-            plain: plainLyrics, 
-            synced: syncedLyrics
-        });
+        const plainLyrics = fetchLyrics.data.plainLyrics;
+        res.json(plainLyrics);
 
     } catch(err){
         console.error('error in lyrics_get: ', err.message);
 
         if(err.response){
-            return req.status(err.response.status).json({
+            return res.status(err.response.status).json({
                 error: 'Lyrics not found for this song.'
             });
         }
-
-        req.status(500).json({ error: 'Internal server error' })
+        res.status(500).json({ error: 'Internal server error' })
     }
-
-
-    
 };
 
 module.exports = { lyrics_get };
