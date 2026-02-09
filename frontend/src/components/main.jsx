7d7cfdf83bddaefe835d79c4992c44a9955b14a2
Wendy <139488528+WendyDarli@users.noreply.github.com>
2026-02-09 20:55:45 -0300

    feat: set up QueryClient for React Query

--- frontend/src/components/main.jsx
+++ frontend/src/components/main.jsx
@@ -5,8 +5,19 @@ import { createBrowserRouter, RouterProvider } from 'react-router';
 import routes from '../routes/routes.jsx';
 const router = createBrowserRouter(routes);
 
+// TANSTACK QUERY CLIENT
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-      <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />   
+      <ReactQueryDevtools initialIsOpen={false} />   
+    </QueryClientProvider>
+
   </StrictMode>
 )
