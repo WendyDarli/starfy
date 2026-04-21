dff932fcb5062f10e9d0d6c5e452ff31c0e1a364
wendydarli <139488528+WendyDarli@users.noreply.github.com>
2026-04-21 16:37:12 -0300

    refactor(album): extract service layer

--- backend/controllers/albumsController.js
+++ backend/controllers/albumsController.js
@@ -1,40 +1,9 @@
-const { formatSpotifyData } = require('../utils/formatSpotifyData');
-const spotifyApi = require('../config/axiosConfig');
-const formatSpotifyItems = require('../utils/formatSpotifyItems');
-const checkFavoriteStatus = require('../utils/checkFavoriteStatus')
+const asyncHandler = require('../utils/asyncHandler');
+const getAlbumDataWithTracks = require('../services/spotify/getAlbumDataWithTracks');
 
-async function album_get(req, res){
-    try{
-        const id = req.params.id;
-                
-        const albumData = await spotifyApi.get(`/albums/${id}`);
-        const albumTracks = await spotifyApi.get(`/albums/${id}/tracks?limit=50`);
-        
-        let items = formatSpotifyItems(albumTracks.data.items, item => ({
-            isFavorite: false
-        }));
-        items = await checkFavoriteStatus(items);
-
-        const response = formatSpotifyData({
-            title: 'Album',
-            name: albumData.data.name,
-            images: albumData.data.images,
-            owner: albumData.data.artists?.[0]?.name,
-            total: albumTracks.data.total,
-            followers: null,
-            items: items
-        });
-
-        return res.json(response);
-        
-    }catch(err){
-        console.error('error at album_get:', err);
-        return res
-        .status(err.response?.status || 500)
-        .json({
-            message: err.response?.data?.error || err.message || 'Error fetching albums.',
-        });
-    };
-};
+const album_get = asyncHandler(async (req, res, next) => {
+    const response = await getAlbumDataWithTracks(req.params.id);
+    return res.json(response);
+});
 
 module.exports = { album_get };
---
+++ backend/services/spotify/getAlbumDataWithTracks.js
@@ -0,0 +1,36 @@
+const { formatSpotifyData } = require('../../utils/formatSpotifyData');
+const spotifyClient = require('../../api/axiosConfig');
+const formatSpotifyItems = require('../../utils/formatSpotifyItems');
+const checkFavoriteStatus = require('../../utils/checkFavoriteStatus')
+const getLoggerContext = require('../../utils/getLoggerContext');
+
+async function getAlbumDataWithTracks(id){
+    const logger = getLoggerContext({albumsId: id, action: 'getAlbumDataWithTracks'})
+    logger.info('Fetching album data from Spotify');
+
+    const [albumData, albumTracks] = await Promise.all([
+        spotifyClient.get(`/albums/${id}`),
+        spotifyClient.get(`/albums/${id}/tracks?limit=50`)
+    ])
+    
+    let items = formatSpotifyItems(albumTracks.data.items, item => ({
+        isFavorite: false
+    }));
+
+    items = await checkFavoriteStatus(items);
+
+    const response = formatSpotifyData({
+        title: 'Album',
+        name: albumData?.data?.name ?? 'Unknow',
+        images: albumData?.data?.images ?? [],
+        owner: albumData?.data?.artists?.[0]?.name ?? 'Unknow',
+        total: albumTracks?.data?.total ?? 0,
+        followers: null,
+        items: items
+    });
+
+    return response;
+};
+
+module.exports = getAlbumDataWithTracks;
+
