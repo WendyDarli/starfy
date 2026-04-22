3f9f0e00017d63f5577ac735d8d5163111e43bdb
wendydarli <139488528+WendyDarli@users.noreply.github.com>
2026-04-22 00:27:25 -0300

    refactor(playlist): extract service layer

--- backend/controllers/playlistsController.js
+++ backend/controllers/playlistsController.js
@@ -1,44 +1,9 @@
-const spotifyApi = require('../config/axiosConfig');
-const formatSpotifyData = require('../utils/formatSpotifyData');
-const formatSpotifyItems = require('../utils/formatSpotifyItems');
-const checkFavoriteStatus = require('../utils/checkFavoriteStatus');
+const asyncHandler = require('../utils/asyncHandler');
+const getPlaylistDataWithTracks = require('../services/spotify/playlists/getPlaylistDataWithTracks')
 
-async function playlist_get(req, res){
-    try{
-        const id = req.params.id;
-        const playlistData = await spotifyApi.get(`/playlists/${id}`, {
-            params: { fields: 'name,images,owner.display_name,tracks.total,id' }
-        });
-        const playlistTracks = await spotifyApi.get(`/playlists/${id}/tracks?limit=50`, {
-            params: { fields: 'items(added_at,track(id,name,artists(name,id),album(id,name,images),duration_ms,external_ids))' }
-        });
-
-        let items = formatSpotifyItems(playlistTracks.data.items, item => ({
-            isFavorite: false
-        }));
-        items = await checkFavoriteStatus(items);
-
-        const response = formatSpotifyData({
-            title: 'Playlist',
-            name: playlistData.data.name,
-            images: playlistData.data.images,
-            owner: playlistData.data.owner?.display_name,
-            total: playlistTracks.data.total,
-            followers: null,
-            playlistId: playlistData.data.id,
-            items: items,
-        });
-
-        return res.json(response);
-
-    } catch(err){
-        console.error('error in playlist_get: ', err)
-        return res
-        .status(err.response?.status || 500)
-        .json({
-           message: err.response?.data?.error || err.message ||  'Error fetching user playlist songs.',
-        });
-    }
+async function playlist_get(req, res, next){
+    const response = await getPlaylistDataWithTracks(req.params.id);
+    res.json(response);
 };
 
 module.exports = { playlist_get };
---
+++ backend/services/spotify/playlists/getPlaylistDataWithTracks.js
@@ -0,0 +1,37 @@
+const spotifyClient = require('../../../api/axiosConfig');
+const getLoggerContext = require('../../../utils/getLoggerContext');
+const formatSpotifyItems = require('../../../utils/formatSpotifyItems');
+const formatSpotifyData = require('../../../utils/formatSpotifyData');
+const checkFavoriteStatus = require('../../../utils/checkFavoriteStatus');
+
+async function getPlaylistDataWithTracks(id) {
+    const logger = getLoggerContext({playlistId: id, action: 'getPlaylistDataWithTracks'})
+    logger.info('Fetching playlist data from Spotify');
+
+    const [playlistData, playlistTracks] = await Promise.all([
+        await spotifyClient.get(`/playlists/${id}`),
+        await spotifyClient.get(`/playlists/${id}/tracks?limit=50`)
+    ]);
+
+
+    let items = formatSpotifyItems(playlistTracks.data.items, item => ({
+            isFavorite: false
+    }));
+
+    items = await checkFavoriteStatus(items);
+
+    const response = formatSpotifyData({
+        title: 'Playlist',
+        name: playlistData?.data?.name ?? 'Playlist Name',
+        images: playlistData?.data?.images ?? [],
+        owner: playlistData?.data?.owner?.display_name ?? 'Unknow',
+        total: playlistTracks?.data?.total ?? 0,
+        followers: null,
+        playlistId: playlistData?.data?.id ?? null,
+        items: items,
+    });
+    
+    return response;
+};
+
+module.exports = getPlaylistDataWithTracks;
